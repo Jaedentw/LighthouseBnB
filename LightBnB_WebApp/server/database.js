15d816f49503459d6a97e64a32481511cbc051ec
Jaeden West <jaeden.t.west@gmail.com>
2022-06-20 16:03:12 -0600

    refactors getAllReservations of database.js

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -59,14 +59,27 @@ const addUser =  function(user) {
 }
 exports.addUser = addUser;
 
-//.query(`
-//INSERT INTO users (name, email, password) 
-//VALUES (${user.name}, ${user.email}, ${user.password});`
-
 
 //Get all reservations for a single user.
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`
+    SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2`, 
+    [guest_id, limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.getAllReservations = getAllReservations;
 
