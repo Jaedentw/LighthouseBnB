bd61f7d536c9aa09b5872de381690c719278256a
Jaeden West <jaeden.t.west@gmail.com>
2022-06-20 17:20:30 -0600

    refactors addProperty function of database.js

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -43,7 +43,6 @@ exports.getUserWithId = getUserWithId;
 
 //Add a new user to the database.
 const addUser =  function(user) {
-  console.log(user);
   return pool
     .query(`
     INSERT INTO users (name, email, password) 
@@ -138,19 +137,20 @@ const getAllProperties = (options, limit = 10) => {
 };
 exports.getAllProperties = getAllProperties;
 
-/*
-WHERE city LIKE '%ancouv%'
-  GROUP BY properties.id
-  HAVING avg(property_reviews.rating) >= 4
-  ORDER BY cost_per_night
-  LIMIT $2
-*/
 
 //Add a property to the database
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool
+    .query(`
+    INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`, 
+    [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+    .then((result) => {
+      console.log(result.rows[0]);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.addProperty = addProperty;
